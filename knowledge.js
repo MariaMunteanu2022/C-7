7db02d111b8f8a629ba30710280d6b1ef4b265bc
MariaMunteanu2022 <101243569+MariaMunteanu2022@users.noreply.github.com>
2022-03-21 15:37:17 +0200

    Add files via upload

---
+++ knowledge.js
@@ -0,0 +1,16 @@
+const question = prompt("What's your favorite movie ?")
+if (question === "Batman") {
+    console.log(`My favorite movie is ${question}`)
+}
+else {
+    console.log("Not")
+}
+var age = prompt(`What is your age?`)
+    if (age > 16){
+        console.log(`You are eligible`) 
+    } 
+    else {
+        console.log(`You must be at least 16 years old. It seams you are ${age}`) 
+    }
+
+
